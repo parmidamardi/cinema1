d8eca4dadb827b8fbcb592d2c1397b2aba1d96d9
parmidamardi <parmidamardi@users.noreply.github.com>
2020-01-03 14:12:38 +0330

    movie struct filled

--- cinema.c
+++ cinema.c
@@ -76,14 +76,38 @@ struct Auditorium
     char movieName[100];
     struct Sons TodaySons[7];
 };
-struct movie
+struct Movie
 {
     char movieName[200];
     int movieTime;
     char summary[10000];
     char movieJenre[100];
 };
-
+struct Movie movieFill(struct Movie M,int num)
+{
+    if(num == 0)
+    {
+        strcpy(M.movieName,"The Green Mile");
+        M.movieTime = 2;
+        strcpy(M.summary,"Paul Edgecomb, the head guard of a prison, meets an\n inmate, John Coffey, a black man who is accused of\n murdering two girls. His life changes drastically when\n he discovers that John has a special gift.");
+        strcpy(M.movieJenre,"crime drama");
+    }
+    else if(num == 1)
+    {
+        strcpy(M.movieName,"The prestige");
+        M.movieTime = 3;
+        strcpy(M.summary,"Two friends and fellow magicians become bitter\n enemies after a sudden tragedy. As they devote\n themselves to this rivalry, they make sacrifices that\n bring them fame but with terrible consequences.");
+        strcpy(M.movieJenre,"psychological thriller");
+    }
+    else if(num == 2)
+    {
+        strcpy(M.movieName,"shutter island");
+        M.movieTime = 2;
+        strcpy(M.summary,"Teddy Daniels and Chuck Aule, two US marshals, are\n sent to an asylum on a remote island in order to\n investigate the disappearance of a patient, where\n Teddy uncovers a shocking truth about the place.");
+        strcpy(M.movieJenre,"neo-noir psychological thriller");
+    }
+    return M;
+}
 int main()
 {
     char time[200];
@@ -92,6 +116,10 @@ int main()
     date(time);
     todayDate(time,today);
     clockTime(time,&hour,&min,&sec);
+    struct Movie M[3];
+    M[0] = movieFill(M[0],0);
+    M[1] = movieFill(M[1],1);
+    M[2] = movieFill(M[2],2);
     //printf("%d : %d : %d",hour,min,sec);
     //printf("%s",today);
 }
